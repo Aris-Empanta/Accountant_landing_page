3112dcc97f8d397d49cfed45f1b6eaaa5aa1d937
Aris Empanta <eams220891@gmail.com>
2022-07-04 18:21:09 +0300

    Starting insurance section

--- src/components/Insurance.js
+++ src/components/Insurance.js
@@ -1,8 +1,10 @@
 import "./css/Insurance.css"
+import insuranceSection from "./images/insuranceSection.jpg"
 
 export const Insurance = () => {
 
     return <div>
-                Insurance
+                <img id="insuranceSectionPhoto" src= { insuranceSection } />
+                <h1 id="insuranceHeader">Insurance</h1>
            </div>
 }
